7e2a771b388171b071b7dcc4af498bca060302ba
Fernando R P Marques <frpm1001@gmail.com>
2021-10-11 13:02:47 -0300

    Add booking routes and controllers, fix n+1 queries from booking and teacher models

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :teachers, only: %i[index show create destroy]
+      resources :bookings, only: %i[index create destroy]
     end
   end
 
-  root to: 'teachers#index'
+  root 'api/v1/teachers#index'
   
 end
