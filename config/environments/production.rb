5327361531057b886ed6b03d2b1069cf17d08a37
Fernando R P Marques <frpm1001@gmail.com>
2021-10-21 22:41:48 -0300

    Update correct cors and default_url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  Rails.application.default_url_options[:host] = 'https://quirky-goldwasser-fe1024.netlify.app/'
+  Rails.application.default_url_options[:host] = 'https://book-a-teacher.herokuapp.com'
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
