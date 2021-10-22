3eb1cebada28ce7b8596c7552df92bd939b2a516
Fernando R P Marques <frpm1001@gmail.com>
2021-10-22 17:26:53 -0300

    Allow cors from any origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://quirky-goldwasser-fe1024.netlify.app'
+    origins '*'
 
     resource '*',
              headers: :any,
